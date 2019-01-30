40bcf35bdd8697b5f58fd7162eccda10ff29e3c2
Dima <33123184+DimaMolod@users.noreply.github.com>
2019-01-30 14:16:17 +0100

    output filename is passed as an argument

--- stat.py
+++ stat.py
@@ -1,69 +1,71 @@
 #!/usr/bin/env python
+# coding: utf-8
+
 import numpy as np
 import saxsdocument
 import os
 import sys
 import glob
-import matplotlib.pyplot as plt
-import math
 import time
 
-outFileName = "mean.dat"
-
 start_time = time.time()
+def main(outFileName, *args):
+    #args = glob.glob("data/*.dat")
+    Is = np.array([])
+    s = np.array([])
+    lenS = []
+    i = 0
+    for file in args:
+        if outFileName in file:
+            continue
+        doc = saxsdocument.read(file)
+        dat = np.transpose(np.array(doc.curve[0]))
+        i+=1
+        if i == 1:
+            s = dat[0]
+            Is = dat[1]
+        else:
+            scat_vector = dat[0]
+            intensity = dat[1]
+            Is = np.vstack((Is,intensity))
+            s = np.vstack((s,scat_vector))
+            lenS.append(len(scat_vector))
 
-args = iter(sys.argv)
-next(args)
-#debug
-#args = glob.glob("data/*.dat")
-Is = np.array([])
-s = np.array([])
-lenS = []
-i = 0
-for file in args:
-    doc = saxsdocument.read(file)
-    dat = np.transpose(np.array(doc.curve[0]))
-    i+=1
-    if i == 1:
-        s = dat[0]
-        Is = dat[1]
+    np.info(Is)
+    In = np.transpose(Is)
+    sPoints= Is.shape[1]
+    len_n =min(lenS)
+    mean_i = np.zeros(sPoints)
+    stdv_i = np.zeros(sPoints)
+
+    i=0
+    for n in range(0, sPoints):
+        av_i = np.mean(In[n])
+        st_i = np.std(In[n])
+        #av_i = sum(In[n])
+        mean_i[i]=av_i
+        stdv_i[i]=st_i
+        i+=1
+
+    mean_dat = np. vstack((s[0], mean_i, stdv_i))
+
+    np.savetxt(outFileName,np.transpose(mean_dat), fmt='%f')
+
+    print ("Averaged file is written..."+outFileName)
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        raise SyntaxError("Insufficient arguments")
     else:
-        scat_vector = dat[0]
-        intensity = dat[1]
-        Is = np.vstack((Is,intensity))
-        s = np.vstack((s,scat_vector))
-        lenS.append(len(scat_vector))
-
-np.info(Is)
-In = np.transpose(Is)
-sPoints= Is.shape[1]
-len_n =Is.shape[0]
-
-mean_i = np.zeros(sPoints)
-stdv_i = np.zeros(sPoints)
-
-i=0
-for n in range(0, sPoints):
-    av_i = np.mean(In[n])
-    st_i = np.std(In[n])
-    #av_i = sum(In[n])
-    mean_i[i]=av_i
-    stdv_i[i]=st_i
-    i+=1
-
-#fig, ax = plt.subplots(
-
-#ax.plot(s[0], mean_i)
-#ax.plot(s[0], stdv_i)
-#ax.set(xlabel='S, inv. nm', ylabel='A.u.',
-#       title='Aver. intensities and standart deviations over a SEC-SAXS dataset')
-#plt.show()
-mean_dat = np. vstack((s[0], mean_i, stdv_i))
-
-np.savetxt(outFileName,np.transpose(mean_dat), fmt='%f')
-
-print ("Averaged file is written..."+outFileName)
-print("--- %s seconds ---" % (time.time() - start_time))
-
-
-
+        args = iter(sys.argv)
+        isArgument = False
+        for arg in args:
+            if "=" in arg:
+                a = arg.split("=")[0]
+                n = arg.split("=")[1]
+                if (a == "-o"):
+                    main(n, *sys.argv[2:])
+                    isArgument = True
+        if (isArgument == False):
+            main("mean.dat", *sys.argv[2:])
